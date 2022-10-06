26202dfe692197986a2188979a19f82960051b0e
Mahmuda_Munny <mahmuda_munny@gmail.com>
2022-10-07 01:08:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ const MyButton = document.querySelector(".MyButton button");
+ const RulesBox = document.querySelector("RulesBox");
+
+
+
+MyButton.onclick = () => {
+	RulesBox.classList.add("activeInfo");
+}
