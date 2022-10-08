8398e372f519fa547514de19bb70a5e8ed38bd93
Mahmuda_Munny <mahmuda_munny@gmail.com>
2022-10-09 01:04:46 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
  const MyButton = document.querySelector(".MyButton button");
  const RulesBox = document.querySelector(".RulesBox");
-
-
+ const exit = document.querySelector(".buttons .exit");
+ const ContinueButton = document.querySelector(".buttons .ContinueButton");
+ const Questions = document.querySelector(".Questions");
 
 MyButton.onclick = () => {
 	RulesBox.classList.add("activeInfo");
+}
+
+exit.onclick = () =>{
+	RulesBox.classList.remove("activeInfo");
+}
+
+ContinueButton.onclick = () => {
+	RulesBox.classList.remove("activeInfo");
+	Questions.classList.add("activeQuiz");
 }
