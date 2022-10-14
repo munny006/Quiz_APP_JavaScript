720c301d5e9197952cd40dd2b29d58b88d2893de
Mahmuda_Munny <mahmuda_munny@gmail.com>
2022-10-15 00:45:46 +0600

    first commit

---
+++ js/question.js
@@ -0,0 +1,74 @@
+let questions = [
+	{
+		numb:1,
+		question : 'What Does HTML Stand For?',
+		anser:  'Hyper Text Markup Language',
+		option:[
+
+		   "Hyper Text Preprocessors",
+		   "Hyper Text Markup Language",
+		   "Hyper Text Multiple Language",
+		   "Hyper Tool Multi Language"
+
+		]
+	},
+{
+		numb:2,
+		question : 'What Does CSS Stand For?',
+		anser:  'Cascading style sheet',
+		option:[
+
+		   "common style sheet",
+		   "colorful style sheet",
+		   "computer style sheet",
+		   "Cascading style sheet"
+
+		]
+	},
+{
+		numb:3,
+		question : 'What Does PHP Stand For?',
+		anser:  'Cascading style sheet',
+		option:[
+
+		   "common style sheet",
+		   "colorful style sheet",
+		   "computer style sheet",
+		   "Cascading style sheet"
+
+		]
+	},
+
+
+	{
+		numb:4,
+		question : 'What Does SQL Stand For?',
+		anser:  'Cascading style sheet',
+		option:[
+
+		   "Stylish Question Language",
+		   "Stylisheet Query Language",
+		   "Statement Question Language",
+		   "Structured Query Language"
+
+		]
+	},
+
+
+	{
+		numb:5,
+		question : 'What Does XML Stand For?',
+		anser:  'Cascading style sheet',
+		option:[
+
+		   "eXtensible Markup Language",
+		   "eXecutable Multiple Language",
+		   "eXtra Multi-program Language",
+		   "eXamine Multiple Language"
+
+		]
+	},
+
+
+
+]
--- js/script.js
+++ js/script.js
@@ -15,4 +15,34 @@ exit.onclick = () =>{
 ContinueButton.onclick = () => {
 	RulesBox.classList.remove("activeInfo");
 	Questions.classList.add("activeQuiz");
+	showQuestions(0);
+}
+
+let que_count = 0;
+
+const nextBtn = document.querySelector(".nextBtn");
+nextBtn.onclick = () => {
+	if(que_count < questions.length - 1){
+		que_count ++ ;
+		showQuestions(que_count)
+	}
+	else{
+		console.log("You Have Completd Your Task") ;
+	}
+}
+
+
+function showQuestions(index){
+	const Quetext = document.querySelector('.text');
+	const Option_list = document.querySelector(".MyOption");
+	let option_tag = '<div class="options">'+ questions[index].option[0]  +'</div>'
+					+ '<div class="options">'+ questions[index].option[1]  +'</div>'
+					+ '<div class="options">'+ questions[index].option[2]  +'</div>'
+					+ '<div class="options">'+ questions[index].option[3]  +'</div>'
+	let que_tag = "<span>" + questions[index].numb+ '.'+ questions[index].question + "</span>";
+	Quetext.innerHTML = que_tag;
+	Option_list.innerHTML = option_tag;
+	const total_que = document.querySelector('.total_que');
+	let total_quetag = '<p>' + questions[index].numb +  ' of 5 </p>'
+	total_que.innerHTML = total_quetag;
 }
